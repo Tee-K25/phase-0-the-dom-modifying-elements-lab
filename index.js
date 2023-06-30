4389157f28b21555447d4883f4bc92b277f26200
Tony <tony.kiplagat@student.moringaschool.com>
2023-06-30 10:03:11 +0300

    Done with DOM

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+main.remove();
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+document.body.append(newHeader);
+newHeader.textContent = "Tony is the champion";
